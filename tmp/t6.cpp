5e12f4e56f76506cc3b019758094e1eeb2b2c134
hanhansoul <whr2290@gmail.com>
2014-05-08 11:27:20 +0800

    5.8 new

---
+++ tmp/t6.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#define DEBUG 0
+
+using namespace std;
+
+/*
+ * */
+
+int main()
+{
+    #if DEBUG
+    freopen("input_data", "r", stdin); 
+    #endif
+
+    
+    return 0;
+}
+
